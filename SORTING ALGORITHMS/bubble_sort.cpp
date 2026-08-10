2661e3fee8606623863b8c9f5c608f0eba883ee4
Hamiza Khan <hamizaa137@gmail.com>
2026-08-10 20:16:58 +0500

    by bubble sorting

---
+++ SORTING ALGORITHMS/bubble_sort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//bubble sorting
+void bubble_sort(vector<int> & arr, int n){
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j+1]);
+            }
+            
+        }
+        
+    }
+    
+}
+
+int main()
+{
+    cout<<"*** BUBBLE SORTING ***"<<endl;
+    int n;
+    cout<<"ENTER SIZE OF AN ARRAY: ";
+    cin>> n;
+
+    vector<int> arr(n);
+    cout<<"ENTER "<<n<<" ELEMENTS: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    bubble_sort(arr, n);
+    cout<<"SORTED ARRAY: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
